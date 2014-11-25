9dbae092b193253e829d4ed99c843897b5313f0b
Ingo Bürk <ingo.buerk@tngtech.com>
2014-11-25 23:02:51 +0100

    replace sh script with python script

---
+++ .i3/conky-i3bar.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+
+def write(message):
+  sys.stdout.write(message + '\n')
+  sys.stdout.flush()
+
+def read():
+  try:
+    line = sys.stdin.readline().strip()
+    if not line:
+      sys.exit(3)
+    return line
+  except KeyboardInterrupt:
+    sys.exit()
+
+if __name__ == '__main__':
+  write('{ "version": 1, "stop_signal": 10, "cont_signal": 12, "click_events": true }')
+  write('[[]')
+
+  os.system('conky -d -c ~/.conkyrc')
+
+  while True:
+    line, prefix = read(), ''
+    if line.startswith(','):
+      line, prefix = line[1:], ','
+
+    parsed = json.loads(line)
+
+    x, y = parsed['x'], parsed['y']
+    module = parsed['name']
+
+    if module == 'calendar':
+      os.system('gsimplecal')
