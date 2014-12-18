373cc64cfb0781243e546034995b6348dc83e7aa
Ingo Bürk <ingo.buerk@tngtech.com>
2014-12-18 12:54:17 -0500

    #2: added some utils to get active window via i3-msg

--- scripts/conkyrc.py
+++ scripts/conkyrc.py
@@ -6,6 +6,7 @@ import json
 import netifaces
 import basiciw
 import datetime
+from jsonpath_rw import jsonpath, parse
 
 SCRIPT_DIR = '$HOME/scripts/'
 
@@ -106,6 +107,34 @@ def run(command):
 
 def run_script(command):
   return run(SCRIPT_DIR + command)
+
+def i3_msg(type):
+  ''' Executes i3-msg -t <type> and returns the parsed JSON output.  '''
+  return json.loads(run('i3-msg -t ' + type)[0])
+
+def get_workspace():
+  ''' Returns the node of the currently focused workspace. '''
+  workspaces = i3_msg('get_workspaces')
+  tree = i3_msg('get_tree')
+
+  focused_num = None
+  for workspace in workspaces:
+    if workspace['focused']:
+      focused_num = workspace['num']
+      break
+
+  for node in tree['nodes'][1]['nodes'][1]['nodes']:
+    if node['num'] == focused_num:
+      return node
+
+  return None
+
+# TODO remove a flag which one is the active window
+def get_window_titles():
+  ''' Returns an array of window titles of the current workspace.  '''
+
+  expression = parse("$..window_properties.title")
+  return [match.value for match in expression.find(get_workspace())]
 
 #########################################
 ### MODULES #############################
