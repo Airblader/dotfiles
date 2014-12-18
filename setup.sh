99f851305db6c7af8df96046adfcce63e2b887fe
Airblader <ingo.buerk@tngtech.com>
2014-12-18 15:54:32 -0500

    #3: improved setup script

--- setup.sh
+++ setup.sh
@@ -26,4 +26,24 @@ sudo apt-get install -y ${DEPS_COMPTON}
 
 sudo pip3 install ${DEPS_PYTHON3}
 
-# TODO symlinks
+dir=`pwd`
+if [ ! -e "${dir}/${0}" ]; then
+  echo "Script not called from within repository directory. Aborting."
+  exit 2
+fi
+
+# TODO ask for everything
+ln -sfn ${dir}/.gitconfig ${HOME}/.gitconfig
+ln -sfn ${dir}/.conkyrc ${HOME}/.conkyrc
+ln -sfn ${dir}/.bashrc ${HOME}/.bashrc
+ln -sfn ${dir}/.vimrc ${HOME}/.vimrc
+ln -sfn ${dir}/.Xresources ${HOME}/.Xresources
+ln -sfn ${dir}/.compton.conf ${HOME}/.compton.conf
+
+ln -sfn ${dir}/.i3 ${HOME}/.i3
+ln -sfn ${dir}/.vim ${HOME}/.vim
+ln -sfn ${dir}/.bash.d ${HOME}/.bash.d
+
+ln -sfn ${dir}/scripts ${HOME}/scripts
+
+sudo cp ${dir}/etc/sudoers.d/power-control /etc/sudoers.d/
